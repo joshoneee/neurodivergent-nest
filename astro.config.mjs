59b90ef13c28632b7ee7b02a89278d5eda4aaae0
j <joshhella84@gmail.com>
2026-04-27 00:41:29 +1000

    feat: switch to GitHub Actions + GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://neurodivergent-nest.vercel.app',
+  site: 'https://joshoneee.github.io',
+  base: '/neurodivergent-nest',
   integrations: [tailwind()],
 });
